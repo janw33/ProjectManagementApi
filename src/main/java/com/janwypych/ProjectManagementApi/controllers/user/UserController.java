fae8fe45ca782f2bff1b94784feaf9a9eacac170
Jan Wypych <32uczen32@gmail.com>
2026-07-17 15:33:34 +0200

    Add Contoller GetCurrentUserTests

--- src/main/java/com/janwypych/ProjectManagementApi/controllers/user/UserController.java
+++ src/main/java/com/janwypych/ProjectManagementApi/controllers/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("/api/v1/account")
 public class UserController {
     private final UserService userService;
 
---
+++ src/test/java/com/janwypych/ProjectManagementApi/controllers/user/GetCurrentUserTests.java
@@ -0,0 +1,87 @@
+package com.janwypych.ProjectManagementApi.controllers.user;
+
+import com.janwypych.ProjectManagementApi.BaseTest.user.BaseTestUser;
+import com.janwypych.ProjectManagementApi.TestDataUtil;
+import com.janwypych.ProjectManagementApi.dtos.user.UserResponse;
+import com.janwypych.ProjectManagementApi.entities.user.User;
+import com.janwypych.ProjectManagementApi.services.user.UserService;
+import com.janwypych.ProjectManagementApi.services.workspace.WorkspaceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GetCurrentUserTests extends BaseTestUser {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    private Authentication createAuthentication() {
+        return new UsernamePasswordAuthenticationToken(
+                user,
+                null,
+                null
+        );
+    }
+
+    private RequestPostProcessor authenticatedUser() {
+        return authentication(createAuthentication());
+    }
+
+    private void performGet(ResultMatcher... matchers) throws Exception {
+        var result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/v1/account")
+                        .with(authenticatedUser())
+        );
+
+        for (ResultMatcher matcher : matchers) {
+            result.andExpect(matcher);
+        }
+    }
+
+    @Test
+    public void shouldReturn401WhenUserIsUnauthenticated() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/account"))
+                .andExpect(
+                        status().isUnauthorized()
+                );
+    }
+
+    @Test
+    public void shouldReturn200WhenRequestIsValid() throws Exception {
+        UserResponse userResponse = UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .createdAt(user.getCreatedAt())
+                .build();
+
+        when(userService.getCurrentUser(any(User.class)))
+                .thenReturn(userResponse);
+
+        performGet(
+                status().isOk(),
+                jsonPath("$.id").value(user.getId()),
+                jsonPath("$.username").value(user.getUsername()),
+                jsonPath("$.email").value(user.getEmail()),
+                jsonPath("$.createdAt").isNotEmpty());
+    }
+}
